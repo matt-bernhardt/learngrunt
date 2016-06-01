d0511f781f160d3f250a2bbfde8762c660c12015
Matthew Bernhardt <matt.j.bernhardt@gmail.com>
2016-05-31 22:04:08 -0400

    Adds grunt tasks to look up latest git commit and store as thiversion (#1)
    Adds grunt tasks to look up latest git commit and store as this.version

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,55 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    revision: {
+      options: {
+        property: 'meta.revision',
+        ref: 'HEAD',
+        short: true
+      },
+      preprocess: {
+        options: {
+          context: {
+            revision: '<%= meta.revision %>'
+          }
+        }
+      }
+    },
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: 'release',
+              replacement: '<%= meta.revision %>'
+            }
+          ]
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['src/learngrunt.js'],
+          dest: 'build/',
+        }]
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
+        src: 'build/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
     }
   });
 
-  // Load the plugin that provies the "uglify" task.
+  // Load the plugin that provides the tasks.
+  grunt.loadNpmTasks('grunt-git-revision');
+  grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('release', ['revision', 'replace'])
+  grunt.registerTask('default', ['release', 'uglify']);
 }
